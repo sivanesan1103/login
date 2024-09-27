cb5d6e9a4372b0049c12bf6b0e2fd576b9a9ca1a
sivanesan1103 <sivanesan1103@gmail.com>
2024-09-27 22:12:52 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -29,74 +29,159 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   double height = 10.0;
 
   TextEditingController controller = TextEditingController(text: "Data Ready");
-
   @override
   Widget build(BuildContext context) {
     double sHeight = MediaQuery.of(context).size.height;
     double sWidth = MediaQuery.of(context).size.width;
     double rHeight =
         sHeight - (sHeight * 0.04) - MediaQuery.of(context).padding.top;
+
     return Scaffold(
-      // backgroundColor: Colors.red.withOpacity(0.4),
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        leading: Icon(Icons.add),
-        title: Text("data"),
-        actions: [Icon(Icons.read_more), Icon(Icons.delete), Icon(Icons.add)],
+        leading: Icon(Icons.arrow_back, color: Colors.white),
+        backgroundColor: Colors.transparent,
+
+
       ),
-      body: Container(
-        color: Colors.transparent,
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("data"),
-              TextField(
-                controller: controller,
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                  'https://img.freepik.com/free-photo/vivid-blurred-colorful-wallpaper-background_58702-3937.jpg',
+                ),
+                fit: BoxFit.cover,
               ),
-              Text("data2"),
-              TextField(
-                controller: controller,
-              ),
-              Text("data3"),
-              TextField(
-                controller: controller,
-              ),
-            ],
+            ),
           ),
-        ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.only(top: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () async {
-                  // Navigator.pop(context);
-                  await Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => NewPage()));
-                  debugPrint("Data Executed");
-                  setState(() {
-                    controller.text = "Data Updated";
-                  });
-                },
-                child: Text("data"))
-          ],
-        ),
+          Align(
+            alignment: Alignment(0, -0.2),
+            child: Container(
+              width: 100,
+              height: 100,
+              child: Image.network(
+                'https://d8it4huxumps7.cloudfront.net/uploads/images/150x150/645751b4d2389_infotechlogo.jpg?d=200x200',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height / 2 - 60,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.8),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                  bottomLeft: Radius.zero,
+                  topRight: Radius.zero,
+                  bottomRight: Radius.zero,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 20),
+                    Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text('Authorised ID', style: TextStyle(color: Colors.black)),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter your Authorised ID here',
+                        suffixIcon: Icon(Icons.person,color: Colors.blueAccent,),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text('Password', style: TextStyle(color: Colors.black)),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: 'Enter your password here',
+                        suffixIcon: Icon(Icons.lock,color: Colors.blueAccent),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Add your login functionality here
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          padding: EdgeInsets.symmetric(vertical: 16.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: Text('Login'),
+                      ),
+                    ),
+
+
+
+                    SizedBox(height: 20),
+                    Center(
+                      child: RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "Don't have an account? ",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                              ),
+                            ),
+                            TextSpan(
+                              text: "Sign up now!",
+                              style: TextStyle(
+                                color: Colors.blue, // Change this to your desired color
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold, // Optional: make it bold
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
-}
+  }
